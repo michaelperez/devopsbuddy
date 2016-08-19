2272aa525b3afbb64d2241bc00bafff10eb1971d
michaelperez <michael.perez@yahoo.com>
2016-08-19 18:33:50 -0500

    Created the JPA entity model for Spring Security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,70 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+	
+	private String name;
+	
+	 @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	 private Set<UserRole> userRoles = new HashSet<UserRole>();
+	
+	public Role() {
+		
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+	}
+	
+	
+}
--- src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
+++ src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
@@ -1,5 +1,7 @@
 package com.devopsbuddy.config;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,4 +19,11 @@ public class DevelopmentConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	
+	@Bean
+    public ServletRegistrationBean h2ConsoleServletRegistration() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
+    }
 }
